57e2c6ed025a1731c8e1973eb3f2aab986d25669
Guillermo Esteban García <guillermo@MacBook-Pro-de-Guillermo.local>
2024-01-21 14:30:45 +0100

    Exercise 6

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,15 +1,49 @@
 // escena.component.ts
 import { Component, Input } from '@angular/core';
 import { iStep } from '../istep';
+import { trigger, transition, query, style, animate, group } from '@angular/animations';
+
+const left = [
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  group([
+    query(':enter', [style({ transform: 'translateX(-100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
+      optional: true,
+    }),
+    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(100%)' }))], {
+      optional: true,
+    }),
+  ]),
+];
+
+const right = [
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  group([
+    query(':enter', [style({ transform: 'translateX(100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
+      optional: true,
+    }),
+    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(-100%)' }))], {
+      optional: true,
+    }),
+  ]),
+];
 
 @Component({
   selector: 'app-escena',
   templateUrl: './escena.component.html',
-  styleUrls: ['./escena.component.css']
+  styleUrls: ['./escena.component.css'],
+  animations: [
+    trigger('animSlider', [
+      transition(':increment', right),
+      transition(':decrement', left),
+    ]),
+  ],
 })
+
+
 export class EscenaComponent {
   @Input() steps: iStep[] = [];
   currentStep: number = 0;
+
 
   nextStep() {
     if (this.currentStep < this.steps.length - 1) {
